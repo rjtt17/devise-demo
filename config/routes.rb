57dbb5f2ee7dbb07f94df1e221e858b28815f773
rjtt17 <rjtt17@gmail.com>
2021-09-24 19:23:24 +0900

    deviseを使用してユーザー認証を実装とbookのcrud作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :books
+  root to: 'home#index'
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
